a8811e40afa399d55981e38440480361ca3b20b2
deartis <deartissolu@gmail.com>
2025-04-18 18:40:31 -0300

    Iniciando o login

--- frontend_old/src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CardArea } from "@/components/Dashboard/CardArea";
-
-export default function Dashboard() {
-    return (
-        <>
-            <CardArea />
-        </>
-    );
-}
--- frontend_old/src/services/whatsapp.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const whatsapp = axios.create({
-    baseURL: `https://whatsapp.gnswhatssender.com.br`,
-})
-
-export default whatsapp;
